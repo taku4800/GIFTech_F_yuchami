d39e4abda4569f403007ce9e6c01a8a92bbbc8f7
noam1813 <devgobouman@gmail.com>
2024-04-14 01:40:36 +0900

    スワイプ時の動作を刷新

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,71 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { ImageBackground, View, Text, Dimensions, Image } from 'react-native';
-import TinderCard from 'react-tinder-card';
+import React, { useEffect, useState } from 'react';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
+import { PanResponderGestureState, Image } from 'react-native';
+import { fetchRemindItem } from '../services/Service';
 import styles from '../styles/screens/HomeScreen.style';
-import { fetchRemindItem, postXXA, postXXB } from '../services/Service';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-export default function Home() {
-  // APIから取得した確認リスト
+const TinderAnimation: React.FC = () => {
   const [remindItemStates, setRemindItemStates] = useState<RemindItem[]>([]);
   const [childRefs, setChildRefs] = useState<React.RefObject<any>[]>([]);
 
   const screen = Dimensions.get('window');
   const specificImageInfo = require('../../assets/chara/GF1.png');
+  const SWIPE_THRESHOLD = 120;
+
+  const [pan] = useState(new Animated.ValueXY());
+  const createPanResponder = (id: number) => {
+    return PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          {
+            dx: pan.x,
+            dy: pan.y,
+          },
+        ],
+        { useNativeDriver: false },
+      ),
+      onPanResponderRelease: (
+        _: any,
+        gestureState: PanResponderGestureState,
+      ) => {
+        if (gestureState.dx > 120 || gestureState.dx < -120) {
+          // カードを元の位置に戻すアニメーション
+          Animated.timing(pan, {
+            toValue: {
+              x: gestureState.dx + gestureState.moveX,
+              y: screen.height,
+            }, // 画面外の位置まで移動
+            duration: 500, // アニメーションの持続時間
+            useNativeDriver: false,
+          }).start(() => {
+            outOfFrame(id);
+            console.log('OK');
+            pan.setValue({ x: 0, y: 0 });
+          });
+        } else {
+          Animated.spring(pan, {
+            toValue: { x: 0, y: 0 },
+            useNativeDriver: false,
+          }).start();
+        }
+      },
+    });
+  };
+
+  const rotateCard = pan.x.interpolate({
+    inputRange: [-200, 0, 200],
+    outputRange: ['-30deg', '0deg', '30deg'],
+  });
+
+  const animatedStyle = {
+    transform: [
+      { translateX: pan.x },
+      { translateY: pan.y },
+      { rotate: rotateCard },
+    ],
+  };
 
   useEffect(() => {
     // APIから確認リストを取得する
@@ -26,22 +79,6 @@ export default function Home() {
     };
     fetchData();
   }, []);
-
-  // TinderCardを左右のどちらかにスワイプしたら発動
-  const swiped = async (direction: any, id: number) => {
-    console.log('removing: ' + id + ' to the ' + direction);
-    // directionに応じた送信処理
-    switch (direction) {
-      case 'left':
-        await postXXA();
-        break;
-      case 'right':
-        await postXXB();
-        break;
-      default:
-        break;
-    }
-  };
 
   // TinderCardが画面外に移動したら発動
   const outOfFrame = (id: number) => {
@@ -57,33 +94,26 @@ export default function Home() {
       style={[styles.container, { width: screen.width, height: screen.height }]}
     >
       <View style={[{ width: screen.width * 0.9, height: screen.width * 0.9 }]}>
-        {remindItemStates.map((character, index) => (
-          <TinderCard
-            ref={childRefs[index]}
-            key={character.id}
-            onSwipe={(dir) => swiped(dir, character.id)}
-            onCardLeftScreen={() => outOfFrame(character.id)}
-          >
-            <ImageBackground
+        {remindItemStates.map((character, index) =>
+          index === remindItemStates.length - 1 ? (
+            <Animated.View
+              {...createPanResponder(character.id).panHandlers}
+              key={character.id}
+              style={[animatedStyle]}
+            >
+              <Image style={styles.cardImage} source={{ uri: character.url }} />
+            </Animated.View>
+          ) : (
+            <Image
+              key={character.id}
               style={styles.cardImage}
               source={{ uri: character.url }}
             />
-            {index === remindItemStates.length - 1 && (
-              <Image
-                source={specificImageInfo} // 画像のパスを指定
-                style={[
-                  styles.cardChara,
-                  {
-                    width: screen.width * 0.3,
-                    height: screen.width * 0.3,
-                    top: -screen.width * 0.15,
-                  },
-                ]}
-              />
-            )}
-          </TinderCard>
-        ))}
+          ),
+        )}
       </View>
     </View>
   );
-}
+};
+
+export default TinderAnimation;
