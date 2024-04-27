c4df7b3a25d40e50278fd4930eee490050874144
UEC-Natori <n2330088@edu.cc.uec.ac.jp>
2024-04-28 03:08:35 +0900

    background handler implemented

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Home from './src/screens/HomeScreen';
-import { LogBox } from 'react-native';
+import { setupBackgroundHandler, handleAppStateChange } from './src/backgrounds/BackgroundHandler';
+import { LogBox, AppState } from 'react-native';
 
 export default function App() {
   LogBox.ignoreAllLogs();
-  return <Home />;
+  const [appKey, setAppKey] = useState(0);
+
+  useEffect(() => {
+    setupBackgroundHandler();
+    const unsubscribe = handleAppStateChange(() => {
+      setAppKey(prevKey => prevKey + 1); // アプリがフォアグラウンドに戻ったときにキーを更新
+    });
+    return unsubscribe; // クリーンアップ関数として返す
+  }, []);
+
+  return <Home key={appKey} />;
 }
